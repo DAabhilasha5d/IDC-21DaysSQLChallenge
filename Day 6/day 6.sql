31052b270b211e97b922415b658a7868d492b24e
Abhilasha Dhole <130473135+DAabhilasha5d@users.noreply.github.com>
2025-11-09 00:42:37 +0530

    Add files via upload

---
+++ Day 6/day 6.sql
@@ -0,0 +1,25 @@
+-- Practice Questions:
+
+-- 1. Count the number of patients by each service.
+SELECT service, COUNT(DISTINCT(patient_id)) AS No_of_patients  FROM patients
+GROUP BY service;
+
+-- 2. Calculate the average age of patients grouped by service.
+SELECT service, ROUND(AVG(age), 0) AS Avg_age FROM patients
+GROUP BY service;
+
+-- 3. Find the total number of staff members per role.
+SELECT role, COUNT(DISTINCT(staff_id)) AS Total_staff FROM staff
+GROUP BY role;
+
+/*Daily Challenge:
+Question: For each hospital service, calculate the total number of patients admitted, total patients refused, 
+and the admission rate (percentage of requests that were admitted). Order by admission rate descending.*/
+
+SELECT service, 
+SUM(patients_admitted) AS Total_admitted,
+SUM(patients_refused) AS Total_refused,
+ROUND((SUM(patients_admitted) / SUM(patients_request)) * 100, 2) AS admission_rate
+FROM services_weekly
+GROUP BY service
+ORDER BY admission_rate DESC;
